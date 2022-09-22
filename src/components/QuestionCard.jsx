e11e479edc33accc2c354a0d9df50ae9cb26fae4
Lucas Ng <lucasngyileang@gmail.com>
2022-09-22 11:28:49 +0800

    feat: add free text input for evaluation

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,31 +1,52 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { dispatch } from '../store';
 
-function QuestionCard({ question, subdomain }) {
-  return (
-    <div className=" w-2/3 mx-auto m-5 flex flex-row justify-between">
-      <p className="max-w-md">{question}</p>
-      <div className="w-48 ml-auto p-0">
-        <select
-          className="select w-full max-w-xs"
-          onChange={(e) =>
-            dispatch.evaluation.setResponse({
-              qn: question,
-              subdomain,
-              resp: e.target.value,
-            })
-          }
-        >
-          <option selected>Not Assessed</option>
-          <option>Not Yet</option>
-          <option>Not Proficient</option>
-          <option>Average</option>
-          <option>Very Good</option>
-        </select>
+function QuestionCard({ question, subdomain, questionType = 'dropdown' }) {
+  if (questionType === 'dropdown') {
+    return (
+      <div className=" w-2/3 mx-auto m-5 flex flex-row justify-between">
+        <p className="max-w-md">{question}</p>
+        <div className="w-48 ml-auto p-0">
+          <select
+            className="select w-full max-w-xs"
+            onChange={(e) =>
+              dispatch.evaluation.setResponse({
+                qn: question,
+                subdomain,
+                resp: e.target.value,
+              })
+            }
+          >
+            <option selected>Not Assessed</option>
+            <option>Not Yet</option>
+            <option>Not Proficient</option>
+            <option>Average</option>
+            <option>Very Good</option>
+          </select>
+        </div>
+
       </div>
-    </div>
-  );
+    );
+  } else if (questionType === 'text_area') {
+    return (
+      <div className=" w-2/3 mx-auto m-5 flex flex-col justify-between">
+        <p className="max-w-md">{question}</p>
+        <div className="w-full p-0 mt-5">
+          <textarea
+            onChange={(e) =>
+              dispatch.evaluation.setResponse({
+                qn: question,
+                subdomain,
+                resp: e.target.value,
+              })
+            }
+            className="textarea textarea-bordered w-full"
+            placeholder="Type here..."
+          ></textarea>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default QuestionCard;
