b4fe709d855baf9a469cec585737893225e70125
Lucas Ng <lucasngyileang@gmail.com>
2022-09-22 17:38:47 +0800

    feat: add report model

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import childPhoto from '../assets/child_photo.webp';
 import CommentsTable from '../components/report/CommentsTable';
 import Graph from '../components/report/Graph';
 import RatingsTable from '../components/report/RatingsTable';
+import StudentSelection from '../components/StudentSelection';
 
 function Report() {
+  const student = useSelector((state) => state.report.student);
+
+  if (!student) {
+    return <StudentSelection type={'report'} />;
+  }
+
   return (
-    <div className='mx-9'>
+    <div className="mx-9">
       <div className="flex flex-row m-9 justify-center">
         <img src={childPhoto} alt="" className="w-48" />
         <div className="ml-3">
-          <p className="font-bold text-xl">MaryAnn d/o Verasamy </p>
+          <p className="font-bold text-xl">{student}</p>
           <p className="">Centre: Yishun Little Seeds </p>
           <p className="">Class: K2, Class Grace </p>
           <p className="">Date of birth: 1 Jan 2017</p>
